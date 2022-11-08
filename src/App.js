d40416ff4000c1a2c9625d102d16c39d70f8c858
georgewood749 <georgewood749@gmail.com>
2022-11-08 17:36:58 +0000

    added API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import fetchAPI from './actions';
 
 function App() {
+
+let myData = fetchAPI('georgewood749')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub Repo Tracker</h1>
     </div>
   );
 }
