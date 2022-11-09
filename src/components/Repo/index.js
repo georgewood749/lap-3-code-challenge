37d4dab401a44a6d8443288c36fd419635faaa1f
georgewood749 <georgewood749@gmail.com>
2022-11-09 16:57:20 +0000

    repos displaying

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Repo() {
     return (
-        <div>Repo</div>
+        <div>
+            <h1>Repo name</h1>
+            <p></p>
+        </div>
     )
 }
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axiosFetchAPI from '../../actions';
 
 
@@ -15,37 +15,67 @@ export default function SearchForm() {
         const input = e.target.value
         setInputData(input)
     }
-    useEffect(()=>{
-        console.log("inputdata:", inputData)
-    },[inputData])
+    // useEffect(() => {
+    //     console.log("inputdata:", inputData)
+    // }, [inputData])
+
+    async function getData() {
+        const fetchApi = "https://api.github.com/users/georgewood749/repos?per_page=100&page=1"
+        try {
+            const apiData = await fetch(fetchApi);
+            const data = await apiData.json();
+            console.log(data);
+            const repoNames = data.map(repo => repo.name)
+            setGitData(repoNames)
+        } catch (err) {
+            console.error(err)
+        }
+    }
 
     useEffect(() => {
-        try{
-            async function fetchData(){
-                const data = await axiosFetchAPI(inputData)
-                return data.map(item => ({
-                    id: item.id,
-                    repoName: item.name,
-                    username: item.owner.login,
-                    avatar: item.owner.avatar_url
-                }))
-            }    
-        const d = fetchData()
-        setGitData(d)
-        console.log(gitData)
-        } catch(err){
-            console.error(err.message)
-        }        
-     }, [])
+        getData()
+    }, [])
 
-  return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Search user here..." onChange={updateInput}></input>
-            <input type="submit"></input>
-        </form>
-    </div>
-  )
+    // useEffect(() => {
+    //     try {
+    //         async function fetchData() {
+    //             const data = await axiosFetchAPI("oscr1998")
+    //             // const json = JSON.stringify(data)
+    //             // console.log("json:", json)
+    //             console.log("data: ", data[0])
+    //             setGitData(data)
+    //             return data.map(item => ({
+    //                 id: item.id,
+    //                 repoName: item.name,
+    //                 username: item.owner.login,
+    //                 avatar: item.owner.avatar_url
+    //             }))
+    //         }
+    //         const d = fetchData()
+    //         setGitData("d", d)
+    //         console.log("gitData", gitData)
+    //     } catch (err) {
+    //         console.error(err.message)
+    //     }
+    // }, [inputData])
+
+
+    // const val = gitData[0]
+    // console.log("val:", val)
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Search user here..." onChange={updateInput}></input>
+                <input type="submit"></input>
+            </form>
+            {/* {gitData && gitData.map(b => <li>{b.name}</li>)} */}
+        {/* {console.log("gitData2: ", gitData)} */}
+        {gitData.map(b => <li>{b}</li>)}
+
+        </div>
+    )
+
 }
 // console.log("axiosData:", axiosData)
 // console.log("axiosData number of repos:", axiosData.length)
