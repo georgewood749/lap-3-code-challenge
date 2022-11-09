cade691e6731dc171a9e5f7d5a160ef816297c19
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-11-09 14:46:55 +0000

    header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
 import axiosFetchAPI from './actions';
+import Header from './layout/header';
 
-export default async function App() {
-let axiosData = await axiosFetchAPI('georgewood749')
-console.log("axiosData:", axiosData)
-console.log("axiosData number of repos:", axiosData.length)
-const value = Object.values(axiosData)[0]
-console.log("axiosData value:", value)
-console.log("axiosData username:", value.owner.login)
-console.log("axiosData id:", value.id)
+export default function App() {
+// let axiosData = await axiosFetchAPI('georgewood749')
+// console.log("axiosData:", axiosData)
+// console.log("axiosData number of repos:", axiosData.length)
+// const value = Object.values(axiosData)[0]
+// console.log("axiosData value:", value)
+// console.log("axiosData username:", value.owner.login)
+// console.log("axiosData id:", value.id)
 
 // let myData = fetchAPI('georgewood749')
 // console.log("mydata:", myData)
 
   return (
     <div className="App">
+      <Header/>
       <h1>GitHub Repo Tracker</h1>
     </div>
   );
---
+++ src/layout/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <img src="../components/images/GitHub-Mark-Light-64px.png" alt="github logo"/>
+    </div>
+  )
+}
