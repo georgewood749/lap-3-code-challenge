3ad88c0fa5ae24e4889dd765cc45b388007d8f35
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-11-10 14:25:56 +0000

    updated user info to be on the right side

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -9,9 +9,9 @@ export default function User({ avatar, username, repoNum, followers, following, 
             </div>
             <div className='userInfo'>
                 <p className='repoNum'>Number of repos: {repoNum}</p>
-                <p>Followers: {followers}</p>
-                <p>Following: {following}</p>
-                <p>Create date: {createDate.slice(0, 10)}</p>
+                <p className='repoNum'>Followers: {followers}</p>
+                <p className='repoNum'>Following: {following}</p>
+                <p className='repoNum'>Create date: {createDate.slice(0, 10)}</p>
             </div>
         </div>
     )
