a836f0706d71b4c09ca9bbd5a3dd338bda1df0f6
georgewood749 <georgewood749@gmail.com>
2022-11-10 16:16:00 +0000

    styling, icons

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
+import star from '../images/star.svg';
+import fork from '../images/code-branch.svg';
+
 
 export default function Carousel({ gitData, forks, stargazers, visibility, language, repoCreateDate, repoUpdateDate, repoURL }) {
     var half_length = Math.ceil(gitData.length / 2)
@@ -17,11 +20,11 @@ export default function Carousel({ gitData, forks, stargazers, visibility, langu
                             <Accordion.Header><a href={repoURL[index]}>{b}</a></Accordion.Header>
                             <Accordion.Body className='statsContainer'>
                                 <ul>
-                                    <li className='stats'>Forks: {forks[index]}</li>
-                                    <li className='stats'>Stars: {stargazers[index]}</li>
-                                    <li className='stats'>Create Date: {repoCreateDate[index].slice(0, 10)}</li>
-                                    <li className='stats'>Last updated: {repoUpdateDate[index].slice(0, 10)}</li>
-                                    <li className='stats'>Language: {language[index] ? language[index] : "N/A"}</li>
+                                    <li className='stats'><img className='icons' src={fork} alt='fork'/> Forks: {forks[index]}</li>
+                                    <li className='stats'><img className='icons' src={star} alt='star'/> Stars: {stargazers[index]}</li>
+                                    <li className='stats'> Created: {repoCreateDate[index].slice(0, 10)}</li>
+                                    <li className='stats'> Updated: {repoUpdateDate[index].slice(0, 10)}</li>
+                                    <li className='stats'> Language: {language[index] ? language[index] : "N/A"}</li>
                                 </ul>
                             </Accordion.Body>
                         </Accordion.Item>
@@ -33,12 +36,12 @@ export default function Carousel({ gitData, forks, stargazers, visibility, langu
                         <Accordion.Item eventKey="0" className="statsContainerHeader">
                             <Accordion.Header><a href={repoURL[index]}>{b}</a></Accordion.Header>
                             <Accordion.Body className='statsContainer'>
-                                <ul>
-                                    <li className='stats'>Forks: {forks[index]}</li>
-                                    <li className='stats'>Stars: {stargazers[index]}</li>
-                                    <li className='stats'>Create Date: {repoCreateDate[index].slice(0, 10)}</li>
-                                    <li className='stats'>Last updated: {repoUpdateDate[index].slice(0, 10)}</li>
-                                    <li className='stats'>Language: {language[index] ? language[index] : "N/A"}</li>
+                            <ul>
+                                    <li className='stats'><img className='icons' src={fork} alt='fork'/> Forks: {forks[index]}</li>
+                                    <li className='stats'><img className='icons' src={star} alt='star'/> Stars: {stargazers[index]}</li>
+                                    <li className='stats'> Created: {repoCreateDate[index].slice(0, 10)}</li>
+                                    <li className='stats'> Updated: {repoUpdateDate[index].slice(0, 10)}</li>
+                                    <li className='stats'> Language: {language[index] ? language[index] : "N/A"}</li>
                                 </ul>
                             </Accordion.Body>
                         </Accordion.Item>
--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './UserStyle.css'
+import users from '../images/users-alt.svg';
+import book from '../images/book.svg'
 
 export default function User({ avatar, username, repoNum, followers, following, createDate, URL }) {
     return (
         <div className="userContainer">
             <div className='username'>
-                <h2><a href={URL}><img src={avatar} className="userImg"></img>{username}</a></h2>
+                <h2><a href={URL}><img src={avatar} className="userImg"></img> {username}</a></h2>
             </div>
             <div className='userInfo'>
-                <p className='repoNum'>Number of repos: {repoNum}</p>
-                <p>Followers: <b>{followers}</b> · Following: <b>{following}</b></p>
+                <p className='repoNum'><img className='icons' src={book} alt='repos'/> Number of repos: {repoNum}</p>
+                <p><img className='icons' src={users} alt='followers'/> Followers: <b>{followers}</b> · Following: <b>{following}</b></p>
                 <p>Create date: <b>{createDate.slice(0, 10)}</b></p>
             </div>
         </div>
